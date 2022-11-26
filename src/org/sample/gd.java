4af5a1bef84d66284caa7838688fed25a23388ce
Sudharsanpandiarajan <mr.sudharsanofficial@gmail.com>
2022-11-26 15:12:29 +0530

    D

---
+++ src/org/sample/gd.java
@@ -0,0 +1,23 @@
+package org.sample;
+
+public class gd {
+	
+	public static void main(String[] args) {
+		
+	 int num = 10  , a= 0 , b= 1 , c=0;
+	 
+	 
+	 for (int i = 1; i <= num ; i++) {
+		 
+		 c = a+b;
+		 System.out.println(c);
+		 
+		 a=b;
+		 b=c;
+		
+	}
+	
+		
+	}
+
+}
